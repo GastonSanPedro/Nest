f4b1442d57cbd6e826ba41e4421d6bd16164ae7b
GastonSanPedro <fabian.sanpedro@globant.com>
2022-09-08 15:53:00 -0300

    create pokemon mongo

--- 03-pokedex/src/pokemon/pokemon.service.ts
+++ 03-pokedex/src/pokemon/pokemon.service.ts
@@ -1,11 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, BadRequestException, InternalServerErrorException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose/dist';
+import { Model } from 'mongoose';
+import { Pokemon } from './entities/pokemon.entity';
+
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
 
 @Injectable()
 export class PokemonService {
-  create(createPokemonDto: CreatePokemonDto) {
-    return 'This action adds a new pokemon';
+
+   constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>
+   ){}
+
+
+  async create(createPokemonDto: CreatePokemonDto) {
+    createPokemonDto.name = createPokemonDto.name.toLowerCase();
+    try {
+      const pokemon = await this.pokemonModel.create(createPokemonDto);
+      return pokemon;
+    } catch (error) {
+      if(error.code === 11000){ //cuando consologeas error, ves que el error 11000 es cuando el registro ya existe en la base de dates
+        throw new BadRequestException(`Pokemon exists in db ${JSON.stringify(error.keyValue)}`);
+      }
+      console.log(error);
+      throw new InternalServerErrorException(`Cant't create Pokemons - Check server logs`);
+    }
   }
 
   findAll() {
