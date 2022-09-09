79c3e4e25a9a65f581af76aacdfd521b392de936
GastonSanPedro <fabian.sanpedro@globant.com>
2022-09-09 15:55:23 -0300

    seed complete

--- 03-pokedex/src/seed/seed.service.ts
+++ 03-pokedex/src/seed/seed.service.ts
@@ -1,25 +1,67 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
 import axios, { AxiosInstance } from 'axios';
+import { Model } from 'mongoose';
+import { Pokemon } from '../pokemon/entities/pokemon.entity';
 import { PokeResponse } from './interfaces/poke-response.interface';
 
 @Injectable()
 export class SeedService {
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+  ) {}
+
   private readonly axios: AxiosInstance = axios;
 
   async executeSeed() {
+    await this.pokemonModel.deleteMany({});
+
     const { data } = await this.axios.get<PokeResponse>(
-      'https://pokeapi.co/api/v2/pokemon?limit=10',
+      'https://pokeapi.co/api/v2/pokemon?limit=650',
     );
+
     // data.results.forEach((pokemons) => {   Esta es una manera de mostrarlos o la otra es con distructuring
     //   console.log(pokemons.name, pokemons.url);
     // });
 
-    data.results.forEach(({ name, url }) => {
+    // INSERTANDO DATOS UNO A UNO -------- (no eficiente) De esta manera espera a insertar pokemon uno a uno y es muy lento para insertar muchos datos por eso se usa el Promise.all
+    // data.results.forEach(async ({ name, url }) => {
+    //   const segments = url.split('/');
+    //   const no = +segments[segments.length - 2];
+
+    //   const pokemon = await this.pokemonModel.create({ name, no });
+    //   return pokemon;
+    // });
+
+    // INSERTANDO DATOS CON PROMISE.ALL -----Hace multiples inserciones (no tan eficiente) ----------------------------
+
+    // const insertPromisesArray = [];
+
+    // data.results.forEach(async ({ name, url }) => {
+    //   const segments = url.split('/');
+    //   const no = +segments[segments.length - 2];
+
+    //   insertPromisesArray.push(this.pokemonModel.create({ name, no }));
+    // });
+
+    // await Promise.all(insertPromisesArray);
+
+    //--------------------------------------------------------------------------------
+
+    // FORMA MAS EFICIENTE ------------- INSERTANDO TODOS LOS DATOS DE UNA SOLA VEZ
+
+    const pokemonToInsert: { name: string; no: number }[] = [];
+
+    data.results.forEach(async ({ name, url }) => {
       const segments = url.split('/');
       const no = +segments[segments.length - 2];
-      console.log(name, no);
+
+      pokemonToInsert.push({ name, no });
     });
 
-    return data.results;
+    await this.pokemonModel.insertMany(pokemonToInsert);
+
+    return 'Seed executed';
   }
 }
