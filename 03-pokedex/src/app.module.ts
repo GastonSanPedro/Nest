750b8e8686c33ca0617e9d62fe630588fe76c66f
GastonSanPedro <fabian.sanpedro@globant.com>
2022-09-09 18:07:51 -0300

    variable de entorno

--- 03-pokedex/src/app.module.ts
+++ 03-pokedex/src/app.module.ts
@@ -1,25 +1,31 @@
-import { join } from 'path';
 import { Module } from '@nestjs/common';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { PokemonModule } from './pokemon/pokemon.module';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-import { CommonModule } from './common/common.module';
-import { SeedModule } from './seed/seed.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
 
+import { join } from 'path';
+import { CommonModule } from './common/common.module';
+import { EnvConfiguration } from './config/app.config';
+import { PokemonModule } from './pokemon/pokemon.module';
+import { SeedModule } from './seed/seed.module';
 
 @Module({
   imports: [
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname,'..','public'),
-      }),
+    ConfigModule.forRoot({
+      load: [EnvConfiguration],
+    }), //ES IMPORTANTE EL ORDEN EN DONDE LO COLOCO, AL INICIO
 
-    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon-gaston'),
-    
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+
+    MongooseModule.forRoot(process.env.MONGODB),
+
     PokemonModule,
-    
+
     CommonModule,
-    
-    SeedModule
+
+    SeedModule,
   ],
 })
 export class AppModule {}
--- 03-pokedex/src/pokemon/pokemon.service.ts
+++ 03-pokedex/src/pokemon/pokemon.service.ts
@@ -4,20 +4,26 @@ import {
   InternalServerErrorException,
   NotFoundException,
 } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { InjectModel } from '@nestjs/mongoose/dist';
 import { isValidObjectId, Model } from 'mongoose';
-import { Pokemon } from './entities/pokemon.entity';
 
 import { PaginationDto } from 'src/common/dto/pagination.dto';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
+import { Pokemon } from './entities/pokemon.entity';
 
 @Injectable()
 export class PokemonService {
+  private defaultLimit: number;
+
   constructor(
     @InjectModel(Pokemon.name)
     private readonly pokemonModel: Model<Pokemon>,
-  ) {}
+    private readonly configService: ConfigService,
+  ) {
+    this.defaultLimit = this.configService.get<number>('defaultLimit');
+  }
 
   async create(createPokemonDto: CreatePokemonDto) {
     createPokemonDto.name = createPokemonDto.name.toLowerCase();
@@ -30,7 +36,7 @@ export class PokemonService {
   }
 
   findAll(paginationDto: PaginationDto) {
-    const { limit = 10, offset = 0 } = paginationDto; //seteo por defecot un numero si no viene
+    const { limit = this.defaultLimit, offset = 0 } = paginationDto; //seteo por defecot un numero si no viene
     return this.pokemonModel
       .find()
       .limit(limit)
